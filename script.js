21463c722dac1c903b4a71934072d152fcc18775
JoanGarCed <joan.garced@gmail.com>
2021-05-13 16:39:11 -0600

    Cambios drasticos al portafolio

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
 const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
-const sections = document.querySelectorAll('section');
-const navbarLinks = document.querySelectorAll('.navbar-link');
-const progress = document.querySelector('.progress-bars-wrapper');
-const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [80, 80, 90, 30, 10, 20, 10];
 
-window.addEventListener('scroll', () => {
-    mainFn();
+window.addEventListener("scroll", () => {
+  mainFn();
 });
 
 const mainFn = () => {
-    if(window.pageYOffset >= navbarOffsetTop){
-        navbar.classList.add('sticky');
-    }else{
-        navbar.classList.remove('sticky');
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
     }
+  });
 
-    sections.forEach((section, i)=> {
-        if(window.pageYOffset >= section.offsetTop - 10){
-            navbarLinks.forEach(navbarLink => {
-                navbarLink.classList.remove('change');
-            })
-            navbarLinks[i].classList.add('change');
-        }
-
-
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
     });
-    
-    if(window.pageYOffset + window.innerHeight >= progress.offsetTop){
-        document.querySelectorAll('.progress-percent').forEach((el, i) =>{
-            el.style.width =`${progressBarPercents[i]}%`;
-            el.previousElementSibling.firstElementChild.textContent = progressBarPercents[i];
-        });
-    }
-}
+  }
+};
 
 mainFn();
 
